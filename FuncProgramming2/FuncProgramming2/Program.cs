f1041b7147fe2313443b99fb2539db8dc1bba532
Тимофей Явкин <yakhzhik@gmail.com>
2019-10-15 21:34:49 +0300

    Add first version of second task

--- FuncProgramming2/FuncProgramming2/Program.cs
+++ FuncProgramming2/FuncProgramming2/Program.cs
@@ -10,7 +10,8 @@ namespace FuncProgramming2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(BinarySearch(1, new int[] { 1,2,3,4,5,6,7}, 0, 7));
+            //Console.WriteLine(BinarySearch(1, new int[] { 1,2,3,4,5,6,7}, 0, 7));
+            Console.WriteLine(firstNPerfectNumbers(100000, new int[] { }).Length);
         }
 
         private static int BinarySearch(int searcheable ,int[] sortedArray, int leftBound, int rightBound)
@@ -19,6 +20,19 @@ namespace FuncProgramming2
             return sortedArray[middleValueIndex] == searcheable ? middleValueIndex :
                 sortedArray[middleValueIndex] > searcheable ? BinarySearch(searcheable, sortedArray, middleValueIndex, rightBound) :
                 BinarySearch(searcheable, sortedArray, leftBound, rightBound);
+        }
+
+        private static int[] firstNPerfectNumbers(int n, int[] arrayOfPerfects)
+        {
+            if (n == dividersSum(n, n))
+                arrayOfPerfects.Append(n);
+            return n == 1 ? arrayOfPerfects : firstNPerfectNumbers(--n, arrayOfPerfects);
+        }
+        private static int dividersSum(int n, int i)
+        {
+            if (n == 1)
+                return 0;
+            return i == 1 ? 1 : n % (--i) == 0 ? i : 0 + dividersSum(n, i);   
         }
     }
 }
